ed093e48fd6a4989aa32d4523abef41e5cfba2a4
Peer Wagner <wagnerpeer@gmail.com>
2019-07-16 00:49:55 +0200

    Extract functions and enrich time and location information
    * Extract date calculation function
    * Extract time calculation function
    * Delete duplicate function setMeetupHtml
    * Add duration to time information
    * Add address and city to/from venue information

--- assets/js/meetup.js
+++ assets/js/meetup.js
@@ -2,39 +2,51 @@
 // Embedding Meetup-Events
 
 (function() {
-  var getVenueFromResult, setMeetup, setMeetupHtml;
+  var getVenueFromResult, getDateFromResult, getTimeFromResult, setMeetup, setMeetupHtml;
 
   getVenueFromResult = function(result) {
     var latLng, venue;
     venue = "";
     if (result.venue && result.venue.name) {
-      venue = "Ort: " + result.venue.name;
-      latLng = [result.lat, result.lon];
+      venue = result.venue.name + ", " + result.venue.address_1 + " - " + result.venue.city;
+
     }
     return venue;
   };
 
+  getDateFromResult = function(result) {
+      var general_date = new Date(result.time);
+
+      var date_options = {weekday: "long", day: "2-digit", month: "long", year: "numeric"};
+      var date = general_date.toLocaleDateString("de", date_options);
+
+      return date;
+  };
+
+  getTimeFromResult = function(result) {
+      var general_date = new Date(result.time);
+
+      var time_options = {hour: "2-digit", minute: "2-digit"};
+      var start_time = general_date.toLocaleTimeString("de", time_options);
+
+      var end_date = new Date(result.time + result.duration);
+      var end_time = end_date.toLocaleTimeString("de", time_options);
+
+      var time = start_time + " - " + end_time;
+
+      return time;
+  };
+
   setMeetupHtml = function(result) {
-    $("#nav date").html(new Date(result.time).toLocaleDateString());
-  };
 
+      $("#date").html(getDateFromResult(result));
+      $("#time").html(getTimeFromResult(result));
+      $("#next-meetup #title").html(result.name);
+      $("#next-meetup #venue").html(getVenueFromResult(result));
+      $("#next-meetup #description").html(result.description);
 
-setMeetupHtml = function(result) {
-    var general_date = new Date(result.time)
-
-    var date_options = {weekday: "long", day: "2-digit", month: "long", year: "numeric"};
-    var german_date = general_date.toLocaleDateString("de", date_options);
-    $("#date").html(german_date)
-
-    var german_time = general_date.toLocaleTimeString("de");
-    var time_options = {hour: "2-digit", minute: "2-digit"};
-    $("#time").html(german_time)
-
-    $("#next-meetup #title").html(result.name);
-    $("#next-meetup #venue").html(getVenueFromResult(result));
-    $("#next-meetup #description").html(result.description);
-    return $("#next-meetup #rvsp").attr("href", result.eventUrl);
-  };
+      return $("#next-meetup #rvsp").attr("href", result.eventUrl);
+    };
 
   setMeetup = function() {
     var meetup_url;
